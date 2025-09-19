89f58979c5848fb6708a40b04c74b695ca0a943b
Proutum-Ann <liangproutum@gmail.com>
2025-09-19 10:15:12 -0400

    added random video generator

--- js/script.js
+++ js/script.js
@@ -5,15 +5,29 @@ const quotes = [
 ]
 
 var randomNumber = Math.floor(Math.random() * quotes.length);
-	document.getElementById('college-quote').textContent = '“' + quotes[randomNumber].split(' — ')[0] + '”'
-	console.log(quotes[randomNumber])
+document.getElementById('college-quote').textContent = '“' + quotes[randomNumber] + '”'
+console.log(quotes[randomNumber])
+
+const videos = [
+    `https://www.youtube.com/embed/PI7NHVnnrqQ?si=es7dCdR4ZXjjLIaR`,
+    `https://www.youtube.com/embed/AzLEiDCTuYQ?si=5qx1Iw67dDX0XSJn`,
+    `https://www.youtube.com/embed/MMxgHNLQnko?si=4a9yVWOMliUYzx_O`,
+    `https://www.youtube.com/embed/wFNaTFsHHV8?si=3mvEuaYg8NUdxh7r`,
+    `https://www.youtube.com/embed/zFmB7vWY9oY?si=gXbbvplyMCo4DApM`,
+    `https://www.youtube.com/embed/o-gfhu2uJ_I?si=vUFC-ZIGyxxDxb1A`,
+    `https://www.youtube.com/embed/_0u51uoQ8Xc?si=Xkn_gYIoZOuKQs6t`
+]
+
+var randomVid = Math.floor(Math.random() * videos.length);
+document.getElementById('videoplayer').src = videos[randomVid]
+console.log(videos[randomVid])
 
 document.addEventListener('DOMContentLoaded', () => {
     const courseGal = document.getElementById('featuredCourses')
     const clubGal = document.getElementById('featuredClubs')
 
 
-    // Rendering the courses and clubs
+    // Rendering clubs
 
     const render = clubList => {
         clubGal.innerHTML = ''
