1afe32dfb613d551ecec375bcca9411064c636c9
Proutum-Ann <liangproutum@gmail.com>
2025-09-19 10:18:17 -0400

    updated the top bit slightly

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-const quotes = [
-    `Sol Iustitiae et Occidentem Illustra`,
-    `Curiosity. Knowledge. Purpose.`,
-    `Sun of righteousness, shine upon the West also`
-]
-
-var randomNumber = Math.floor(Math.random() * quotes.length);
-document.getElementById('college-quote').textContent = '“' + quotes[randomNumber] + '”'
-console.log(quotes[randomNumber])
-
-const videos = [
-    `https://www.youtube.com/embed/PI7NHVnnrqQ?si=es7dCdR4ZXjjLIaR`,
-    `https://www.youtube.com/embed/AzLEiDCTuYQ?si=5qx1Iw67dDX0XSJn`,
-    `https://www.youtube.com/embed/MMxgHNLQnko?si=4a9yVWOMliUYzx_O`,
-    `https://www.youtube.com/embed/wFNaTFsHHV8?si=3mvEuaYg8NUdxh7r`,
-    `https://www.youtube.com/embed/zFmB7vWY9oY?si=gXbbvplyMCo4DApM`,
-    `https://www.youtube.com/embed/o-gfhu2uJ_I?si=vUFC-ZIGyxxDxb1A`,
-    `https://www.youtube.com/embed/_0u51uoQ8Xc?si=Xkn_gYIoZOuKQs6t`
-]
-
-var randomVid = Math.floor(Math.random() * videos.length);
-document.getElementById('videoplayer').src = videos[randomVid]
-console.log(videos[randomVid])
 
 document.addEventListener('DOMContentLoaded', () => {
-    const courseGal = document.getElementById('featuredCourses')
+    const quotes = [
+        `Sol Iustitiae et Occidentem Illustra`,
+        `Curiosity. Knowledge. Purpose.`,
+        `Sun of righteousness, shine upon the West also`
+    ]
+
+    var randomNumber = Math.floor(Math.random() * quotes.length);
+    document.getElementById('college-quote').textContent = '“' + quotes[randomNumber] + '”'
+    console.log(quotes[randomNumber])
+
+    const videos = [
+        `https://www.youtube.com/embed/PI7NHVnnrqQ?si=es7dCdR4ZXjjLIaR`,
+        `https://www.youtube.com/embed/AzLEiDCTuYQ?si=5qx1Iw67dDX0XSJn`,
+        `https://www.youtube.com/embed/MMxgHNLQnko?si=4a9yVWOMliUYzx_O`,
+        `https://www.youtube.com/embed/wFNaTFsHHV8?si=3mvEuaYg8NUdxh7r`,
+        `https://www.youtube.com/embed/zFmB7vWY9oY?si=gXbbvplyMCo4DApM`,
+        `https://www.youtube.com/embed/o-gfhu2uJ_I?si=vUFC-ZIGyxxDxb1A`,
+        `https://www.youtube.com/embed/_0u51uoQ8Xc?si=Xkn_gYIoZOuKQs6t`
+    ]
+
+    var randomVid = Math.floor(Math.random() * videos.length);
+    document.getElementById('videoplayer').src = videos[randomVid]
+    console.log(videos[randomVid])
+
     const clubGal = document.getElementById('featuredClubs')
 
 
