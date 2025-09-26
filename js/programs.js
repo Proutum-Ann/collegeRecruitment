2c500a49f9fae44425c4f1ab2c7bf77c37e5717a
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-09-26 01:46:55 +0000

    started doing the programs thing

---
+++ js/programs.js
@@ -0,0 +1,22 @@
+const programs = [
+    {
+        name: 'African Area Studies',
+        majmin: 'Minor',
+        desc: 'The field of African area studies emphasizes continental Africa and embraces all of the major regions of the continent. The interdisciplinary minor in African area studies requires a minimum of six courses, including two African language courses (8 credits), two core courses (6 credits), and two elective courses chosen from among the core courses and the approved course list (6 credits; 20 credits total).',
+    },
+    {
+        name: 'Cell Biology and Neuroscience',
+        majmin: 'Major',
+        desc: 'Imagine studying the role of cellular defects in neurodegenerative disorders such as Alzheimer\'s, or investigating brain activity dynamics underlying learning and decision making. Or what about conducting research in the fields of developmental neurobiology or immunology with outstanding scientists? This is all a reality for Rutgers students.',
+    },
+    {
+        name: 'Health Administration',
+        majmin: 'Major/Minor',
+        desc: 'The health administration major prepares students for entry-level positions leading to an administrative career path in the health care sector by providing broad training in health care management, operations, marketing, finance and more. Health care administration is one of the fastest growing fields.',
+    },
+    {
+        name: 'Statistics',
+        majmin: 'Major/Minor',
+        desc: 'Problem solving, multivariate analysis, probability … explore these concepts and more as a statistics major at Rutgers. Statistics is a science of information gathering, data analysis and decision making. The department offers a major in statistics as well as a joint major in statistics and mathematics. Students looking to apply statistics in industry, government, or in applied areas of graduate study should focus on the statistics major.',
+    },
+]
