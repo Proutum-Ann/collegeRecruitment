54d4d2adc195fedcd6868dc59fce862d9cddf37c
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-09-26 01:13:57 +0000

    i'm losing it

--- js/faculty.js
+++ js/faculty.js
@@ -12,54 +12,54 @@ const staff = [
         subject: 'Center for Women and Work, Program for Disability Research',
         image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Alexander_Eva.jpg?itok=vHQ6EA3i&c=17ffa59d6ad17d30f318cc0cdbc2fa97',
         email: 'ea853@smlr.rutgers.edu'
-    },
+    },/*
     {
         name: 'Mark S. Anner',
         position: 'Dean and Distinguished Professor, Office of the Dean',
         subject: 'Global Supply Chains, Workers\' Rights, Garment Sector, Latin America, Worker-Driven Co-Research',
         image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Anner_Mark_2024.jpg?itok=He7Em6yp&c=e910532ab44ad1f54a337ce4f379dc59',
         email: 'mark.anner@rutgers.edu'
+    },*/
+    {
+        name: 'Amy Bahruth',
+        position: 'Lecturer',
+        subject: 'Labor Studies and Employment Relations',
+        image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Bahruth_Amy.jpg?itok=WNMlwF2D&c=0cbea3df561237efff1815ed275865b1',
+        email: 'abahruth@rutgers.edu'
     },
     {
-        name: '',
-        position: '',
-        subject: '',
-        image: '',
-        email: ''
+        name: 'Bill Bailey',
+        position: 'Student Counselor',
+        subject: 'Labor Studies and Employment Relations',
+        image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Bill_Bailey_web.png?itok=RUZBpHK5&c=d7e5f8e794435d6cc218a13dc4572867',
+        email: 'william.bailey@rutgers.edu'
     },
     {
-        name: '',
-        position: '',
-        subject: '',
-        image: '',
-        email: ''
+        name: 'Mehmet Balkanlioglu',
+        position: 'Lecturer',
+        subject: 'Gender, work, race and ethnicity, immigration, family, religion, social justice, and inequality',
+        image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Mehmet_Balkanlioglu.png?itok=GAn9_N8F&c=66e782f26d091b6db08d76807aa648df',
+        email: 'mehmet.balkanlioglu@rutgers.edu'
     },
     {
-        name: '',
-        position: '',
-        subject: '',
-        image: '',
-        email: ''
+        name: 'Jacob Barnes',
+        position: 'Ph.D. Student',
+        subject: 'Industrial Relations and Human Resource Management',
+        image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Barnes_Jacob.jpg?itok=ASql-1ef&c=af01d56b3a3a2196bc7d79b808e61c00',
+        email: 'jacob.barnes@rutgers.edu'
     },
     {
-        name: '',
-        position: '',
+        name: 'Yesenia Basilio',
+        position: 'Business Manager, Office of the Dean',
         subject: '',
-        image: '',
-        email: ''
+        image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Basilio_Yesenia_2024.jpg?itok=BnCQ3ztO&c=bd2e863d5f8d1b16e91992f3e03e284f',
+        email: 'yeseniab@smlr.rutgers.edu'
     },
     {
-        name: '',
-        position: '',
+        name: 'Virginia (Ginny) Becaccio',
+        position: 'Assistant to the Dean, Office of the Dean',
         subject: '',
-        image: '',
-        email: ''
-    },
-    {
-        name: '',
-        position: '',
-        subject: '',
-        image: '',
-        email: ''
+        image: 'https://smlr.rutgers.edu/sites/default/files/styles/manualcrop_1_1_400/public/Images/Faculty-Staff-Photos/Becaccio_Ginny_2021.jpg?itok=dz7tayNi&c=04152e806faedb1b8812e3e5b6f6c451',
+        email: 'ginnyb@smlr.rutgers.edu'
     },
 ]
